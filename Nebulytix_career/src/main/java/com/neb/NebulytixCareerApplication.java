e401a337ffc07ea159b7c3ec9128d016b79b395f
surajcode7 <skoli151100@gmail.com>
2025-11-04 14:19:18 +0530

    added config package and model mapper

--- Nebulytix_career/src/main/java/com/neb/NebulytixCareerApplication.java
+++ Nebulytix_career/src/main/java/com/neb/NebulytixCareerApplication.java
@@ -1,11 +1,17 @@
 package com.neb;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NebulytixCareerApplication {
 
+	@Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(NebulytixCareerApplication.class, args);
 	}
